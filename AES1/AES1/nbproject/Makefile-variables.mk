1d6b5eed070e666d95e430bd0903f45f7ca40250
Branislav Petrovic <branislp@ntnu.no>
2022-06-19 19:53:14 +0200

    Added standalone AES

---
+++ AES1/AES1/nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU-Linux
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux
+CND_ARTIFACT_NAME_Debug=aes1
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux/aes1
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux/package
+CND_PACKAGE_NAME_Debug=aes1.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux/package/aes1.tar
+# Release configuration
+CND_PLATFORM_Release=GNU-Linux
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux
+CND_ARTIFACT_NAME_Release=aes1
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux/aes1
+CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux/package
+CND_PACKAGE_NAME_Release=aes1.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux/package/aes1.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
